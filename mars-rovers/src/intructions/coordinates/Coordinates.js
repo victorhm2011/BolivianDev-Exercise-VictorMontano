0fed53bcd51aac7960653146ceef84c0975f8a96
Victor Hugo Montano <victor.montano@assuresoftbolivia.com>
2022-01-19 17:01:41 -0400

    [FEA-001]: Add inputs of coordinates

--- mars-rovers/src/intructions/coordinates/Coordinates.js
+++ mars-rovers/src/intructions/coordinates/Coordinates.js
@@ -3,7 +3,21 @@ import './Coordinates.scss';
 function Coordinates() {
     return (
         <div>
-            <input type="text" name="coordinates"/>
+            <div>
+                <label>
+                    Start position:
+                    <input type="text" name="start" />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Coordinates: 
+                    <input type="text" name="coordinates"/>
+                </label>
+            </div>
+            <div>
+                <input type='submit' value='Execute'/>
+            </div>
         </div>
     );
 }
