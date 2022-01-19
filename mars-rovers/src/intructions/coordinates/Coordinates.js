c447be6552c6836170361c1367ba2f5b1bed7c57
Victor Hugo Montano <victor.montano@assuresoftbolivia.com>
2022-01-19 16:18:20 -0400

    [FEA-001]: Add basic input

---
+++ mars-rovers/src/intructions/coordinates/Coordinates.js
@@ -0,0 +1,11 @@
+import './Coordinates.scss';
+
+function Coordinates() {
+    return (
+        <div>
+            <input type="text" name="coordinates"/>
+        </div>
+    );
+}
+
+export default Coordinates;
