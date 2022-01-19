4062a216dc5d833412cbf39da1bd3571b0413bac
Victor Hugo Montano <victor.montano@assuresoftbolivia.com>
2022-01-19 19:59:24 -0400

    [FEA-001]: Add handles of coordinates

--- mars-rovers/src/intructions/coordinates/Coordinates.js
+++ mars-rovers/src/intructions/coordinates/Coordinates.js
@@ -1,25 +1,49 @@
 import './Coordinates.scss';
+import { useState } from "react";
 
 function Coordinates() {
+    //constants
+    const N = 1;
+    const E = 2;
+    const S = 3;
+    const W = 4;
+
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(inputs.coordinates);
+    }
+
     return (
         <div>
-            <div>
-                <label>
-                    Start position:
-                    <input type="text" name="start" />
+            <form onSubmit={handleSubmit}>
+                <label>Start position:
+                <input 
+                    type="text" 
+                    name="start" 
+                    value={inputs.start || ""} 
+                    onChange={handleChange}
+                />
                 </label>
-            </div>
-            <div>
-                <label>
-                    Coordinates: 
-                    <input type="text" name="coordinates"/>
+                <label>Coordinates:
+                    <input 
+                    type="text" 
+                    name="coordinates" 
+                    value={inputs.coordinates || ""} 
+                    onChange={handleChange}
+                    />
                 </label>
-            </div>
-            <div>
                 <input type='submit' value='Execute'/>
-            </div>
+            </form>
         </div>
-    );
+    )
 }
 
 export default Coordinates;
