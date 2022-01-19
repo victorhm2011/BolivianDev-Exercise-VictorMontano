ec5a6b0dce9ee1498038c81943b8e3952d66b55d
Victor Hugo Montano <victor.montano@assuresoftbolivia.com>
2022-01-19 15:57:44 -0400

    [FEA-001]: Add cells to move rovers

--- mars-rovers/src/mars/Mars.js
+++ mars-rovers/src/mars/Mars.js
@@ -1,8 +1,20 @@
+import './Mars.scss';
 
 function Mars() {
 
+    let cells = [1,2,3,4,5,6,7,8,9]
     return (
-        <p>Hello from Mars</p>
+        <div>
+            <div className="mars-container">
+                {
+                    cells.map(cell => (
+                        <div className="mars-container_cell-container">
+                            <p>{cell}</p>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     );
 }
 
