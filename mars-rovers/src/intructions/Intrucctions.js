9c402b3ba83015afc002d28e76c8aeb024337519
Victor Hugo Montano <victor.montano@assuresoftbolivia.com>
2022-01-19 16:11:43 -0400

    [FEA-001]: Add instructions component

---
+++ mars-rovers/src/intructions/Intrucctions.js
@@ -0,0 +1,19 @@
+import './Instrucctions.scss';
+
+function Instrucctions() {
+    return (
+        <div>
+            <div>
+                <h1>Interview Coding Problem</h1>
+            </div>
+            <div>
+                <h2>Mars Rovers problem</h2>
+            </div>
+            <div>
+                <p>By: Victor Hugo Montaño</p>
+            </div>
+        </div>
+    );
+}
+
+export default Instrucctions;
